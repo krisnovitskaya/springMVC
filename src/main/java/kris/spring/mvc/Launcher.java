3818445ca3902a18d1cfbdb07675fd403d532702
Kris Novitskaya <voitage@yandex.ru>
2020-09-15 11:03:06 +0300

    init

---
+++ src/main/java/kris/spring/mvc/Launcher.java
@@ -0,0 +1,25 @@
+package kris.spring.mvc;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+import java.net.URL;
+import java.security.ProtectionDomain;
+
+public class Launcher {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8189);
+
+        ProtectionDomain domain = Launcher.class.getProtectionDomain();
+        URL location = domain.getCodeSource().getLocation();
+
+        WebAppContext webAppContext = new WebAppContext();
+        webAppContext.setContextPath("/mvc");
+        // http://localhost:8189/mvc
+        webAppContext.setWar(location.toExternalForm());
+
+        server.setHandler(webAppContext);
+        server.start();
+        server.join();
+    }
+}
